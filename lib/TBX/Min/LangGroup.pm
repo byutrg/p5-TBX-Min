fc06d6ca4224afcee1766b14a3257135be28c17a
Nathan Glenn <garfieldnate@gmail.com>
2013-11-07 20:40:13 -0800

    created LangGroup.pm with test file
    LangGroup.pm represents a single <langGroup> element of TBX-Min. It
    contains a single scalar (its language), along with an accessor for
    the contained term groups.
    
    I chose to return the internal array reference for the term groups
    because it seemed like it might be unwieldy to also provide
    remove_term_group and clear_term_groups.
    
    The term_groups accessor croaks on input so as to avoid confusion with
    the getter/setter accessors in TermGroup.pm.

---
+++ lib/TBX/Min/LangGroup.pm
@@ -0,0 +1,99 @@
+package TBX::Min::LangGroup;
+use strict;
+use warnings;
+use Carp;
+# VERSION
+
+# ABSTRACT: Store information from one TBX-Min C<langGroup> element
+=head1 SYNOPSIS
+
+    use TBX::Min::LangGroup;
+    my $lang_grp = TBX::Min::LangGroup->new(
+        {lang => 'en'});
+    print $lang_grp->lang(); # 'en'
+    my $term_grp = TBX::Min::TermGroup->new(...);
+    $lang_grp->add_term_group($term_grp);
+    my $term_grps = $lang_grp->term_groups;
+    print $#$term_grps; # '1'
+
+=head1 DESCRIPTION
+
+This class represents a single language group contained in a TBX-Min file.
+A language group is contained by a concept entry, and contains several term
+groups each representing a given concept for the same language.
+
+=cut
+
+=head1 METHODS
+
+=head2 C<new>
+
+Creates a new C<TBX::Min::LangGroup> instance. Optionally you may pass in
+a hash reference which is used to initialize the object. The allowed hash
+fields are C<lang> and C<term_groups>, where C<lang> is the abbreviation of
+the language the contained terms belong to, and C<term_groups> is an array
+reference containing C<TBX::Min::LangGroup> objects.
+
+=cut
+sub new {
+    my ($class, $args) = @_;
+    my $self;
+    if((ref $args) eq 'HASH'){
+        $self = $args;
+    }else{
+        $self = {};
+    }
+    return bless $self, $class;
+}
+
+=head2 C<lang>
+
+Get or set the language group language abbreviation (C<en>, C<de>, etc.).
+
+=cut
+sub lang {
+    my ($self, $lang) = @_;
+    if($lang) {
+        return $self->{lang} = $lang;
+    }
+    return $self->{lang};
+}
+
+=head2 C<term_groups>
+
+Returns an array ref containing all of the C<TBX::Min::TermGroup> objects
+in this concept entry. The array ref is the same one used to store the objects
+internally, so additions or removals from the array will be reflected in future
+calls to this method.
+
+=cut
+sub term_groups {
+    my ($self) = @_;
+    if (@_ > 1){
+        croak 'extra argument found (term_groups is a getter only)';
+    }
+    return $self->{term_groups};
+}
+
+=head2 C<add_term_group>
+
+Adds the input C<TBX::Min::TermGroup> object to the list of language groups
+contained by this object.
+
+=cut
+sub add_term_group {
+    my ($self, $term_grp) = @_;
+    if( !$term_grp || !$term_grp->isa('TBX::Min::TermGroup') ){
+        croak 'argument to add_term_group should be a TBx::Min::TermGroup';
+    }
+    push @{$self->{term_groups}}, $term_grp;
+    return;
+}
+
+=head1 SEE ALSO
+
+L<TBX::Min>
+
+=cut
+
+1;
