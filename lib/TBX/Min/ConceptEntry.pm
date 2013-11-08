9fce3f2f9242bd5fcbff63d04003239847634968
Nathan Glenn <garfieldnate@gmail.com>
2013-11-07 20:46:15 -0800

    created ConceptEntry.pm with test file
    ConceptEntry.pm represents a single <conceptEntry> element of TBX-Min.
    It contains a single scalar (its id), along with an accessor for
    the contained language groups.
    
    I chose to return the internal array reference for the language groups
    because it seemed like it might be unwieldy to also provide
    remove_lang_group and clear_lang_groups.
    
    The lang_groups accessor croaks on input so as to avoid confusion with
    the getter/setter accessors in TermGroup.pm.
    
    The test file checks the constructor and both accessors.

---
+++ lib/TBX/Min/ConceptEntry.pm
@@ -0,0 +1,99 @@
+package TBX::Min::ConceptEntry;
+use strict;
+use warnings;
+use Carp;
+# VERSION
+
+# ABSTRACT: Store information from one TBX-Min C<conceptEntry> element
+=head1 SYNOPSIS
+
+    use TBX::Min::ConceptEntry;
+    my $concept = TBX::Min::ConceptEntry->new(
+        {id => 'B001'});
+    print $concept->id(); # 'B001'
+    my $lang_grp = TBX::Min::LangGroup->new(...);
+    $concept->add_lang_group($lang_grp);
+    my $lang_grps = $concept->lang_groups;
+    print $#$lang_grps; # '1'
+
+=head1 DESCRIPTION
+
+This class represents a single concept entry contained in a TBX-Min file. A
+concept entry contains several language groups, each containing terms that
+represent this concept in a given languages.
+
+=cut
+
+=head1 METHODS
+
+=head2 C<new>
+
+Creates a new C<TBX::Min::ConceptEntry> instance. Optionally you may pass in
+a hash reference which is used to initialize the object. The allowed hash
+fields are C<id> and C<lang_groups>, where C<id> is the concept entry id value
+and C<langGroups> is an array reference containing C<TBX::Min::LangGroup>
+objects.
+
+=cut
+sub new {
+    my ($class, $args) = @_;
+    my $self;
+    if((ref $args) eq 'HASH'){
+        $self = $args;
+    }else{
+        $self = {};
+    }
+    return bless $self, $class;
+}
+
+=head2 C<id>
+
+Get or set the concept entry id value.
+
+=cut
+sub id {
+    my ($self, $id) = @_;
+    if($id) {
+        return $self->{id} = $id;
+    }
+    return $self->{id};
+}
+
+=head2 C<lang_groups>
+
+Returns an array ref containing all of the C<TBX::Min::LangGroup> objects
+in this concept entry. The array ref is the same one used to store the objects
+internally, so additions or removals from the array will be reflected in future
+calls to this method.
+
+=cut
+sub lang_groups {
+    my ($self) = @_;
+    if (@_ > 1){
+        croak 'extra argument found (lang_groups is a getter only)';
+    }
+    return $self->{lang_groups};
+}
+
+=head2 C<add_lang_group>
+
+Adds the input C<TBX::Min::LangGroup> object to the list of language groups
+contained by this object.
+
+=cut
+sub add_lang_group {
+    my ($self, $lang_grp) = @_;
+    if( !$lang_grp || !$lang_grp->isa('TBX::Min::LangGroup') ){
+        croak 'argument to add_lang_group should be a TBx::Min::LangGroup';
+    }
+    push @{$self->{lang_groups}}, $lang_grp;
+    return;
+}
+
+=head1 SEE ALSO
+
+L<TBX::Min>
+
+=cut
+
+1;
---
+++ t/03-ConceptEntry.t
@@ -0,0 +1,42 @@
+# test the functionality of TBX::Min::ConceptEntry
+
+use strict;
+use warnings;
+use Test::More;
+plan tests => 8;
+use Test::Deep;
+use Test::NoWarnings;
+use_ok('TBX::Min::ConceptEntry');
+use TBX::Min::LangGroup;
+use FindBin qw($Bin);
+use Path::Tiny;
+
+my $args = {
+    lang => 'en',
+    lang_groups => [
+        TBX::Min::LangGroup->new({lang => 'en'}),
+        TBX::Min::LangGroup->new({lang => 'zh'}),
+    ],
+};
+
+#test constructor without arguments
+my $concept = TBX::Min::ConceptEntry->new;
+isa_ok($concept, 'TBX::Min::ConceptEntry');
+
+ok(!$concept->id, 'id not defined by default');
+
+#test constructor with arguments
+$concept = TBX::Min::ConceptEntry->new($args);
+is($concept->id, $args->{id}, 'correct id from constructor');
+cmp_deeply($concept->lang_groups, $args->{lang_groups},
+    'correct term groups from constructor');
+
+#test setters
+$concept = TBX::Min::ConceptEntry->new();
+
+$concept->id($args->{id});
+is($concept->id, $args->{id}, 'id correctly set');
+
+$concept->add_lang_group($args->{lang_groups}->[0]);
+cmp_deeply($concept->lang_groups->[0], $args->{lang_groups}->[0],
+    'add_lang_group works correctly');
